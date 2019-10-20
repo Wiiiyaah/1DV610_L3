4e6c1eea9a87b6aab35f6cd55b03f7bafbc0162e
Dawid Kaleta <dawid.kaleta@outlook.com>
2019-10-20 23:37:40 +0200

    Refactoring LayoutView

--- view/LayoutView.php
+++ view/LayoutView.php
@@ -7,6 +7,7 @@ class LayoutView {
    * if a user is logged in or not, the view, date and time.
    */
   public function render($isLoggedIn, $view, DateTimeView $dtv) {
+
     echo '<!DOCTYPE html>
       <html>
         <head>
@@ -16,7 +17,7 @@ class LayoutView {
         </head>
         <body>
           <a href="."><h1>Assignment 3</h1></a>
-          '. $this->addRegisterLink() . '
+          '. $this->getLinkHTML($isLoggedIn) . '
 
           ' . $this->renderIsLoggedIn($isLoggedIn) . '
 
@@ -44,15 +45,41 @@ class LayoutView {
     }
   }
 
+  private function isRegistration() {
+    if (isset($_GET['register'])) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  private function getLinkHTML($isLoggedIn) {
+    if ($this->isRegistration()) {
+      return $this->addRegLink
+    } else if ($isLoggedIn) {
+      return '';
+    } else {
+      return $this->getBackLink;
+    }
+  }
+
   /**
    * Adds a register or back to login link depending on GET-parameter in URL
    * @return - The back- or register link
    */
-  private function addRegisterLink() {
+  private function getRegLink() {
     if (isset($_GET['register'])) {
       return '<a href="./">Back to login</a>';
     } else if (!isset($_SESSION['loggedIn'])) {
       return '<a href="?register">Register a new user</a>';
     }
+  }
+
+  /**
+   * Returns a back to login link as HTML
+   * @return - The back link HTML
+   */
+  private function getBackLink() {
+      return '<a href="./">Back to login</a>';
   }
 }
